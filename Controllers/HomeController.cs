29364b0f5b9f31ab6becca85ec6ede4bd2db6dbd
David Smith <cpmpdavidsmith@icloud.com>
2024-02-21 11:49:27 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AverageCalculator1.Models;
+
+namespace AverageCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalculateAverage(double number1, double number2)
+        {
+            double average = (number1 + number2) / 2;
+            ViewBag.Average = average;
+            return View("Index");
+        }
+    }
+}
